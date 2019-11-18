03fe6e1868686d7536cf8d086c351dcfbbe8fdd9
Caitie N. Pearce <Caitie.N.Pearce@students.ittralee.ie>
2019-11-18 14:03:16 +0000

    Bug fixing

--- app_server/controllers/recipes.js
+++ app_server/controllers/recipes.js
@@ -4,7 +4,7 @@ const apiOptions = {
     server : 'http://localhost:3000' 
     }; 
     if (process.env.NODE_ENV === 'production') { 
-    apiOptions.server = 'https://caitiewf.herokuapp.com/'; 
+    apiOptions.server = 'https://caitiewf.herokuapp.com'; 
     }
     
 /* GET 'home' page */

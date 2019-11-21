8350252796e7b4b595fe7a339a20f234b6fc6790
Caitie N. Pearce <Caitie.N.Pearce@students.ittralee.ie>
2019-11-21 10:58:19 +0000

    Implemented sign up

--- app_api/controllers/accounts.js
+++ app_api/controllers/accounts.js
@@ -17,20 +17,41 @@ const accountsReadOne = function (req, res) {res
 
 const accountsCreate = function(req, res) {
     Us.create({ 
+      
       email: req.body.email,
       password: req.body.password
-      }, (err, user) => { 
+      },(err, user) => {  
+        if (err) {
+          res
+          .status(400)
+          .json(err);
+        }
+
+        else {
+          res
+          .status(201)
+          .json(user);
+        }
+        }
+    )}; 
+      
+      /*(err, user) => { 
+        if(!user){
+          window.alert("email");
+        }
+
       if (err) {
-      res
-      .status(400)
-      .json(err);
-      } else {
-      res
-      .status(201)
-      .json(user);
+        res
+        .status(400)
+        .json(err);
+      } 
+      else {
+        res
+        .status(201)
+        .json(user);
       }
-      });
-      };
+    });
+};*/
       
 
 module.exports = {
--- app_server/controllers/account.js
+++ app_server/controllers/account.js
@@ -1,3 +1,13 @@
+const request = require('request');
+
+
+const apiOptions = { 
+    server : 'http://localhost:3000' 
+    }; 
+    if (process.env.NODE_ENV === 'production') { 
+    apiOptions.server = 'https://caitiewf.herokuapp.com'; 
+    }
+
 const _renderSignUp = function (req, res) { 
     res.render('signup', { title: 'Sign Up' });
 };
@@ -10,13 +20,48 @@ const signUp = function(req, res){
 
 /* PUSH sing up page */
 const doSignUp = function(req, res){ 
-    res.render('signup', { title: 'Sign Up' }); 
+    const path = `/api/signup`; 
+    const postdata = { 
+    email: req.body.email, 
+    password: req.body.pass
+    }; 
+    const requestOptions = {
+        url : apiOptions.server + path, 
+        method : 'POST', 
+        json : postdata 
     };
+    request( requestOptions,(err, response, body) => {
+    if (response.statusCode === 201) { 
+    res.redirect(`/`); 
+    } else { 
+    _showError(req, res, response.statusCode); 
+    }
+    }
+    );
+};
+
 
 /* GET sing up page */
 const logIn = function(req, res){ 
     res.render('logIn', { title: 'Log In' }); 
 };
+
+const _showError = function (req, res, status) {
+    let title = '';
+    let content = '';
+    if (status === 404) { 
+    title = '404, page not found'; 
+    content = 'Oh dear. Looks like we can\'t find this page. Sorry.'; 
+    } else { 
+    title = `${status}, something's gone wrong`; 
+    content = 'Something, somewhere, has gone just a little bit wrong.'; 
+    }
+    res.status(status); 
+    res.render('generic-text', { 
+    title : title, 
+    content : content 
+    }); 
+    };
 
 module.exports = { 
     signUp,
